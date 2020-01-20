ef36bdbbc4a41a58435921a0da6d615c33ef6588
Massimo-stalnoy-strelok-iz-myasa <57673259+Massimo-stalnoy-strelok-iz-myasa@users.noreply.github.com>
2020-01-21 00:31:26 +0300

    Add files via upload

--- flappybird.py
+++ flappybird.py
@@ -1,20 +1,33 @@
 from pygame.locals import *
 import pygame
-from random import randint
+from random import randint, choice
 import sys
+import statistic_db
+import datetime
+import vlc
 
 
 class FlappyBird:
     def __init__(self):
+        a, b = statistic_db.return_status("FlappyOsip")
+        print(b)
+        self.subject = "FlappyOsip"
+        self.set_score = 0
+        self.score = statistic_db.high_score(b, self.subject)
+        self.start_time = datetime.datetime.now().strftime("%H:%M:%S")
+        self.flag = False
+
         self.screen = pygame.display.set_mode((400, 635))
         self.bird = pygame.Rect(65, 50, 50, 50)
 
         self.background = pygame.image.load("flappy.jpg").convert()
-        self.bird_icon = [pygame.image.load("osipenko.png").convert_alpha(),
-                            pygame.image.load("osipenko.png").convert_alpha(),
-                            pygame.image.load("osipenko.png")]
+        self.bird_icon = [pygame.image.load("1.png").convert_alpha(),
+                            pygame.image.load("2.png").convert_alpha(),
+                            pygame.image.load("dead.png")]
         self.uWall = pygame.image.load("bottom.png").convert_alpha()
         self.dWall = pygame.image.load("top.png").convert_alpha()
+        self.songs = ['thriller.mp3', 'axel f.mp3', 'shooting stars.mp3', 'radioactive.mp3']
+        self.p = vlc.MediaPlayer(choice(self.songs))
 
         self.prosvet = 165 # Высота просвета
         self.X_axis = 400 # спавн колонн (уменьшается со временем)
@@ -27,6 +40,7 @@ class FlappyBird:
         self.counter = 0
         self.height = randint(-110, 300)
 
+
     def updateWalls(self):
         self.X_axis -= 2 # стены... они двигаются
         if self.X_axis < -100: # Когда стена уходит за край экрана она
@@ -34,6 +48,7 @@ class FlappyBird:
             self.X_axis = 410
             self.counter += 1 # счёт
             self.height = randint(-110, 300)
+
 
     def cycle(self):
         # А вот тут симуляция физики с поправкой на баланс
@@ -58,9 +73,15 @@ class FlappyBird:
                                self.dWall.get_height())
         # Смэрть
         if uRect.colliderect(self.bird) or dRect.colliderect(self.bird):
+            self.p.stop()
+            self.p = vlc.MediaPlayer('DblA.mp3')
+            self.p.play()
             self.dead = True
 
         if not 0 < self.bird[1] < 720: # если улетела - возраждаем
+            self.p.stop()
+            self.p = vlc.MediaPlayer('DblA.mp3')
+            self.p.play()
             self.bird[1] = 50
             self.Y_axis = 50
             self.dead = False
@@ -68,6 +89,7 @@ class FlappyBird:
             self.X_axis = 410
             self.height = randint(-110, 110)
             self.gravitation = 5
+            self.flag = False
 
     def run(self):
         clock = pygame.time.Clock()
@@ -77,14 +99,47 @@ class FlappyBird:
         while True:
             clock.tick(60)
 
+            if not self.flag:
+                self.screen.fill((255, 255, 255))
+                self.screen.blit(self.background, (0, 0))
+                self.screen.blit(font.render("click mousebutton",
+                                             -1,
+                                             (255, 255, 255)),
+                                 (40, 65))
+                self.screen.blit(font.render("high score: " + str(self.score),
+                                             -1,
+                                             (255, 255, 255)),
+                                 (100, 180))
+                pygame.display.update()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    now = datetime.datetime.now().strftime("%H:%M:%S")
+                    # Время на момент завершения программы
+                    statistic_db.times(self.start_time, self.set_score, now, self.subject)
                     sys.exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN and not self.flag:
+                    self.flag = True
+                    self.p.stop()
+                    self.p = vlc.MediaPlayer(choice(self.songs))
+                    self.p.play()
+
+                if event.type == pygame.KEYDOWN and self.flag:
+                    if event.key == pygame.K_RIGHT:
+                        print(1)
+                    if event.key == pygame.K_LEFT:
+                        print(2)
+
 
                 if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
                     self.jAmplifer = 10
                     self.gravitation = 5
                     self.jSpeed = 10
+
+            if not self.flag:
+                continue
+
             # Строки 95-104 об обновлении экрана, всё стирается и рисуется вновь
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0, 0))
@@ -96,6 +151,7 @@ class FlappyBird:
                                          -1,
                                          (255, 255, 255)),
                              (200, 50))
+
 
             if self.dead:
                 self.sprite = 2 # при смерти замена картинки
@@ -110,9 +166,8 @@ class FlappyBird:
 
             self.updateWalls()
             self.cycle()
+
             pygame.display.update()
-
-
 
 if __name__ == "__main__":
     FlappyBird().run()
